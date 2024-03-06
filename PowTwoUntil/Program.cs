252f1c7ecd72b1ac1f3a53a5fbea37d953772966
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-03-06 20:14:22 +0000

    I added two Powers0f2Until5 in main to call twice

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PowTwoUntil
+{
+    class Program
+    {
+        private static void Powers0f2Until5()
+        {
+            for (int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.Write.Line(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Powers0f2Until5();
+            Powers0f2Until5();
+        }
+    }
+}
