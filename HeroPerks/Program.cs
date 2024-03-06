b322c166f8e9e02f55c744f0cfec14975b34b767
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-03-06 19:48:17 +0000

    checks if there is an argument on the command line

---
+++ HeroPerks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+[Flags]
+enum WoWRoles
+{
+    WaterBreathing = 1
+    Stealth = 2
+    AutoHeal = 4
+    DoubleJump = 8
+}
+
+namespace HeroPerks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("");
+                return;
+            }
+            WoWRoles roles = 0;
+
+            foreach (char l in args[0])
+            {
+                
+            }
+        }
+    }
+}
