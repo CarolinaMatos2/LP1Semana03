2b91b5d49a0e4a2d294e581f081dbc9730ec3ecb
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-03-06 18:28:23 +0000

    does multiplication A * b

---
+++ ArrayMult/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArrayMult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length = 6)
+            {
+                Console.WriteLine("Write six numbers: ");
+                return;
+            }
+            float a11 = float.Parse(args[0]);
+            float a12 = float.Parse(args[1]);
+            float a13 = float.Parse(args[2]);
+            float a14 = float.Parse(args[3]);
+            float b1 = float.Parse(args[4]);
+            float b2 = float.Parse(args[5]);
+            
+        }
+    }
+}
