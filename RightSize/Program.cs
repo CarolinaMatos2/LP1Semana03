9cf5de3643a5d2b354589322691d95e2c22e490e
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-03-06 17:17:58 +0000

    I added foreach and if

---
+++ RightSize/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            foreach(string words in args)
+            {
+                if(words.Length > 6)
+            }
+
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
