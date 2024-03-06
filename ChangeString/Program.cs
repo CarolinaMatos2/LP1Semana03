9666485a051c275fc850e9253bd65ee1eb50e49b
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-03-06 17:58:11 +0000

    I tried using the foreach

---
+++ ChangeString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("String: ");
+            string inst = Console.ReadLine();
+
+            Console.Write("Caráter: ")
+            string substituir = Console.()
+        }
+    }
+}
